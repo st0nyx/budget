fc033c5d7abfcddfebab05e22df1b76fe5f145cc
Bruno Steinmann <bruno.steinmann@gmx.com>
2019-11-11 08:30:12 +0100

    added footer and scope view on home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  def default_url_options
-    { locale: I18n.locale }
-  end
+  # def default_url_options
+  #   { locale: I18n.locale }
+  # end
 
 end
